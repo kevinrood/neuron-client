c053b4d10e873b02dfd1669786d298bc12d0cbe0
Mark Sim <marksim@gmail.com>
2011-08-17 13:15:56 -0500

    Removing 'remote_id' for zones as 'uuid'

--- lib/neuron-client/zone.rb
+++ lib/neuron-client/zone.rb
@@ -4,7 +4,6 @@ module Neuron
       include Connected
       resource_name("zone")
       resources_name("zones")
-      remote_id('uuid')
 
       attr_accessor :slug, :response_type, :template_slug, :parameters,
           :created_at, :updated_at
