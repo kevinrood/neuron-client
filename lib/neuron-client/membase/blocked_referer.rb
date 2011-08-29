0f042572f1c6963e03ad932df03ee4d639df89bb
Kevin Rood <kevinrood@acceletroncorp.com>
2011-08-29 09:57:08 -0400

    NRN-297 -- fetching blocked user agent and referer list from Membase

--- lib/neuron-client/membase/blocked_referer.rb
+++ lib/neuron-client/membase/blocked_referer.rb
@@ -2,6 +2,22 @@ module Neuron
   module Client
     module Membase
       module BlockedReferer
+        def self.included(base)
+          base.extend(ClassMethods)
+        end
+
+        module ClassMethods
+          def all
+            blocked_referers = []
+            cached_json = self.connection.get(all_membase_key)
+            blocked_referers = self.new(Yajl.load(cached_json).collect{|item| BlockedReferer.new(item[self.resource_name])}) if cached_json.present?
+            blocked_referers
+          end
+
+          def all_membase_key
+            'blocked_referers'
+          end
+        end
 
       end
     end
