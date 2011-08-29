e3cfa843a3e966769c47edd64a3dd078d726010c
Kevin Rood <kevinrood@acceletroncorp.com>
2011-08-29 15:02:35 -0400

    NRN-297 -- Adserver (via neuron-client gem) should fetch the blocked user_agent and referer lists/regex from membase.
    - the neuron-client changes are complete, this needs to be implemented in the ad server though.

--- lib/neuron-client/membase/blocked_referer.rb
+++ lib/neuron-client/membase/blocked_referer.rb
@@ -10,7 +10,7 @@ module Neuron
           def all
             blocked_referers = []
             cached_json = self.connection.get(all_membase_key)
-            blocked_referers = self.new(Yajl.load(cached_json).collect{|item| BlockedReferer.new(item[self.resource_name])}) if cached_json.present?
+            blocked_referers = Yajl.load(cached_json).collect{|item| self.new(item[self.resource_name])} if cached_json.present?
             blocked_referers
           end
 
