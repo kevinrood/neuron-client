4287ce398d9b603b6a38db46241ead744829651b
Chris Johnson <chris@kindkid.com>
2011-08-30 14:00:14 -0500

    Include ZoneCalculations in Zone

--- lib/neuron-client/zone.rb
+++ lib/neuron-client/zone.rb
@@ -2,13 +2,17 @@ module Neuron
   module Client
     class Zone
       include Base
+      include ZoneCalculations
 
       resource_name("zone")
       resources_name("zones")
 
       attr_accessor :slug, :response_type, :template_slug, :parameters,
           :created_at, :updated_at, :ad_links
-
+      
+      def find_ad(ad_id)
+        Ad.find(ad_id)
+      end
     end
   end
 end
