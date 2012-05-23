fce98cdd4a99c6730ffcab8459b6f2fa0f577d2d
Chris Johnson <chris@kindkid.com>
2012-05-23 16:09:23 -0500

    NRN-571 - Neuron Reports Includes DART Creative ID

--- lib/neuron-client/model/report.rb
+++ lib/neuron-client/model/report.rb
@@ -3,7 +3,7 @@ module Neuron
     class Report
       include Base
 
-      TEMPLATES = %w(ad_events delivery_metrics post_activities)
+      TEMPLATES = %w(ad_events creative_events delivery_metrics post_activities)
 
       STATES =  %w(NEW WAITING RUNNING READY FAILED CANCELLED)
 
